44dc26bedf4f7c829876626a99157e2a5f9d75bc
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-14 15:35:58 +0200

    able to switch images on screen, one error is cleaned

--- js/script.js
+++ js/script.js
@@ -15,8 +15,9 @@
     const classForImages = 'imagesLine';
 
     const listOfImages = document.querySelector('#imgList');
-    const blurDiv = document.querySelector('#alertZone');
+    const alertZone = document.querySelector('#alertZone');
     const divToShowIMG = document.querySelector('#Content');
+    const zoomedImg = document.querySelector('.imgContent');
 
     const render = () =>{
         listOfImages.innerHTML = '';
@@ -24,10 +25,12 @@
         buttonToAddImage.classList.add('addImgBtn');
         buttonToAddImage.textContent = 'Add new image'
         document.querySelector('body').appendChild(buttonToAddImage);
-        //TODO add to #imgList images 80*80
+
         imgDB.forEach((element)=> {
-            newImg = document.createElement('img');//variant 1
+            newImg = document.createElement('img');
             newImg.src = element;
+
+            newImg.setAttribute('databaseId',imgDB.indexOf(element));
             newImg.classList.add('imagesLine');
             listOfImages.appendChild(newImg);
         });
@@ -38,16 +41,16 @@
     document.addEventListener('click',(event)=>{
         // console.log(event);
         console.log(event.target);
-        if (event.target.classList.contains(addImgBtn)){
+        if (event.target.classList.contains('addImgBtn')){
             useModal();
         }
 
         if (event.target.classList.contains(classForImages)) {
-            // console.log(event);
-            blurDiv.classList.remove('hyde');
+            console.log('class for images');
+            //remove hyde class
+            alertZone.classList.remove('hyde');
             divToShowIMG.classList.remove('hyde');
-            event.target.classList.remove('imagesLine');
-            event.target.classList.add('bigMode');
+            //buttons
             const leftButton = document.createElement("img");
             const rightButton = document.createElement("img");
             leftButton.src = './icon/prev.png';
@@ -56,37 +59,28 @@
             rightButton.classList.add('nextBtn');
             leftButton.classList.add('navBtn');
             rightButton.classList.add('navBtn');
-            blurDiv.appendChild(leftButton);
-            blurDiv.appendChild(rightButton);
-        } else if(event.target.classList.contains('bigMode')){
-            event.target.classList.add('imagesLine');
-            event.target.classList.remove('bigMode');
-            blurDiv.classList.add('hyde');
-            divToShowIMG.classList.add('hyde');
+            alertZone.appendChild(leftButton);
+            alertZone.appendChild(rightButton);
+            //show img with zoom
+            zoomedImg.setAttribute('src',event.target.getAttribute('src'));
+            zoomedImg.setAttribute('databaseId',event.target.getAttribute('databaseId'));
         } else if(event.target.classList.contains('navBtn')){
+            console.log('class NavBtn');
             if(event.pageX < 300){
-                let temp = document.querySelector('.bigMode').previousElementSibling;
-                if (!temp){
-                    const allImg = document.querySelectorAll('.imagesLine');
-                    temp = allImg[allImg.length-1];
-                }
-                document.querySelector('.bigMode').classList.add('imagesLine');
-                document.querySelector('.bigMode').classList.remove('bigMode');
-                temp.classList.add('bigMode');
-                temp.classList.remove('imagesLine')
+                 let prevId = imgDB.indexOf(zoomedImg.getAttribute('src')) - 1;
+                 if (prevId < 0 ) prevId = imgDB.length - 1;
+                zoomedImg.setAttribute('src',imgDB[prevId]);
+
             } else if(event.pageX > (window.innerWidth - 300)){
-                let temp = document.querySelector('.bigMode').nextElementSibling;
-                if (!temp) temp = document.querySelectorAll('.imagesLine')[0];
-                document.querySelector('.bigMode').classList.add('imagesLine');
-                document.querySelector('.bigMode').classList.remove('bigMode');
-                temp.classList.add('bigMode');
-                temp.classList.remove('imagesLine')
+                let nextId = imgDB.indexOf(zoomedImg.getAttribute('src')) + 1;
+                if (nextId => imgDB.length ) nextId = 0;
+                zoomedImg.setAttribute('src',imgDB[nextId]);
+
             }
         } else {
-            blurDiv.classList.add('hyde');
+            console.log('else');
+            alertZone.classList.add('hyde');
             divToShowIMG.classList.add('hyde');
-            document.querySelector('.bigMode').classList.add('imagesLine');
-            document.querySelector('.bigMode').classList.remove('bigMode');
         }
     });
     render();
